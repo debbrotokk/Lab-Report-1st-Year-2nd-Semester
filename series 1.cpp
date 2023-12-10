d811c7bcc96a1ff115287485352993d4d40cc8c5
Debbroto Kumar Karmokar <149927791+debbrotokk@users.noreply.github.com>
2023-12-10 09:23:59 +0600

    Add files via upload

---
+++ series 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the value of n : ";
+    cin >> n;
+    int sum = 0;
+    for (int i = 1; i <= n; ++i)
+    sum += i * (i + 1);
+    cout << "The sum of the series is: " << sum << endl;
+   return 0;
+}
